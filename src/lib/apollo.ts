943457421ebb90781b444bfc6c1df731df3fc9a4
VarnerDamascenoJr <varnerdjr@gmail.com>
2024-06-19 19:12:26 -0300

    feat: removing the auth

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -5,7 +5,7 @@ const client = new ApolloClient({
     link: new HttpLink({
         uri:'https://graphql.contentful.com/content/v1/spaces/wae5kt6i2hgo',
         headers:{
-            Authorization: `Bearer IGVIJb57-GmIGIkMJh2X9_PArbBInAjlUn6zrSMDYyM`,
+            Authorization: `Bearer ${process.env.AUTHORIZATION}`,
         },
         fetch,
     }),
