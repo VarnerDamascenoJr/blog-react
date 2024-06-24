51c0f5a30388e530a3c4cc4a2ad4a2b9fbd20d3b
VarnerDamascenoJr <varnerdjr@gmail.com>
2024-06-24 09:31:49 -0300

    feat: remove of the auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,16 @@ import client from '../lib/apollo';
 import '../app/globals.css';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../lib/next-seo.config';
+import Auth0ProviderWithRouter from '@/lib/auth0Provider';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+   // <Auth0ProviderWithRouter>
     <ApolloProvider client={client}>
         <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </ApolloProvider>
+  //  </Auth0ProviderWithRouter>
   );
 }
 
