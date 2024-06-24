55c127f7cfb90401e452c6f12d1fad5005f08399
VarnerDamascenoJr <varnerdjr@gmail.com>
2024-06-24 11:19:58 -0300

    feat: section fot blog

---
+++ src/components/SectionBlog/index.jsx
@@ -0,0 +1,24 @@
+
+
+
+
+const SectionBlog = ({}) => {
+    return(
+        <section>
+            <div>
+            <p>tag</p>
+            <h3>title</h3>
+            <div>
+                <p>Author and Data</p>
+            </div>
+            <p>sumary</p>
+            <button>Read More</button>
+            </div>
+            <div>
+
+            </div>
+        </section>
+    );
+}
+
+export default SectionBlog;
