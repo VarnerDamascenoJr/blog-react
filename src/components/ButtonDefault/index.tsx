e269214bf34e0a594eddc33dc7ee78af9024c0dc
VarnerDamascenoJr <varnerdjr@gmail.com>
2024-07-10 15:21:25 -0300

    feat: adding button hover

--- src/components/ButtonDefault/index.tsx
+++ src/components/ButtonDefault/index.tsx
@@ -8,7 +8,7 @@ type ButtonProps = {
 const ButtonDefault:React.FC<ButtonProps> = ({text}) => {
     return(
         <button
-        className="font-semibold text-black bg-regular-yellow px-5 py-1"
+        className="font-semibold text-black bg-regular-yellow px-5 py-1 hover:bg-regular-yellow-400"
         >{text}
         </button>
     )
